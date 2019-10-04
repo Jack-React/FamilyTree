06c2b1b19f476c5cf3af9b05bb266bd2e102130f
Jack p <jtv199@gmail.com>
2019-10-04 18:31:31 +1000

    circle drawn

--- networktree/test.js
+++ networktree/test.js
@@ -4,18 +4,35 @@ import {
   StyleSheet,
   ScrollView,
   View,
-  Text,
   StatusBar,
 } from 'react-native';
-
-
+import Svg,{ Circle, G, Path, Text, Rect } from 'react-native-svg';
 class TestApp extends Component{
+  constructor(props){
+    super(props);
+    this.state = {nodes:[]};
+  }
+  render(){
+    return(
+      <Svg height="100" width="100">
+      <View>
+        <Node/>
+      </View>
+    </Svg>
+  );
+  }
+}
+
+class Node extends Component{
   constructor(props){
     super(props);
     this.state = {name:'Frarthur'};
   }
   render(){
-    return<View></View>;
+    return(
+    <G>
+       <Circle cx="50" cy="50" r="50" fill="pink" />
+    </G>);
   }
 }
 
